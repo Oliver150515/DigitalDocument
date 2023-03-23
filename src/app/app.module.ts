84f63529e457ede3a39ce77ba1b351db06351ac6
Oliver Garcia <op14799@gmail.com>
2023-03-22 20:00:10 -0400

    agregando el angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,20 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { AppRoutingModule } from './app-routing.module';
 import { JwtInterceptor } from './interceptors/jwt.interceptor';
 import { CommonModule } from '@angular/common';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatTableModule} from '@angular/material/table';
+
+
 import { LegalizacionesComponent } from './components/legalizaciones/legalizaciones.component';
 import { DocumentosComponent } from './components/documentos/documentos.component';
 import { PerfilComponent } from './components/perfil/perfil.component';
 import { MetodoPagoComponent } from './components/metodo-pago/metodo-pago.component';
 import { InformacionPersonalComponent } from './components/informacion-personal/informacion-personal.component';
 import { EditarPerfilComponent } from './components/perfil/editar-perfil/editar-perfil.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [				
@@ -34,7 +42,12 @@ import { EditarPerfilComponent } from './components/perfil/editar-perfil/editar-
     AppRoutingModule,
     FormsModule,
     CommonModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatTabsModule,
+    MatExpansionModule,
+    MatTableModule,
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true}
